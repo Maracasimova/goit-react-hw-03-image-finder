95677b8e278e02faf37b0068d7fb34632a7aa2c4
Casimova Marina <maracasimova@gmail.com>
2023-05-15 22:50:50 +0300

    Loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,9 +1,28 @@
+import React, { useState, useEffect } from 'react';
 import Loader from 'react-content-loader';
 
 const Spinner = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 5000);
+  }, []);
+
   return (
     <div className="spinner">
-      <Loader type="Oval" color="#3f51b5" height={50} width={50} />
+      {isLoading ? (
+        <Loader
+          height={140}
+          speed={1}
+          backgroundColor={'#d6d2c7'}
+          foregroundColor={'#999'}
+          viewBox="0 0 380 70"
+        />
+      ) : (
+        <div></div>
+      )}
     </div>
   );
 };
